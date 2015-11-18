75c05e6c121b94f3ea4820d4a92933e5aa5f1eb6
Volker Knollmann <foorgol@yahoo.com>
2015-11-18 19:01:01 +0100

    Intermediate, incomplete commit.

--- reports/commonReportElements/MatchMatrix.cpp
+++ reports/commonReportElements/MatchMatrix.cpp
@@ -18,16 +18,13 @@ QRectF MatchMatrix::plot(const QPointF& topLeft)
 
   if ((topLeft.x() < 0) && (topLeft.y() < 0))
   {
-    origin.setY(rep->curY);
-    origin.setX((rep->getPageWidth() - rep->getUsablePageWidth()) / 2.0);
+    origin = rep->getAbsCursorPos();
   }
 
-  rep->drawHorLine(origin.x(), origin.y(), 30);
+  rep->drawHorLine(origin, 30);
   rep->drawHorLine(origin.x() + 30, origin.y(), 30);
-  rep->drawVertLine(origin.x(), origin.y(), 10);
+  rep->drawVertLine(origin, 10);
   rep->drawVertLine(origin.x() + 30, origin.y(), 10);
 
   return QRectF(origin, QSizeF(30, 10));
-
-  return QRectF();
 }
